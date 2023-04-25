33c2e5d2683c29a64897d21c966c6c496f5bf6f2
Mushahid Ali <30910296+MushahidAli@users.noreply.github.com>
2023-04-25 09:38:16 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -62,7 +62,7 @@ const Body = () => {
 
     return(<><div style={{color: 'black', border: '1px solid grey', borderRadius: '5px', backgroundColor: '#D3D3D3'}}>
         <br />GitHub UserName:<br /><br />
-        <input onkeydown={checking(e)} style={{border: '0', padding: '10px', borderRadius: '5px', borderLeft: '3px solid green', backgroundColor: 'black', color: 'white'}} id="github-id" placeholder="UserName" /><br /><br />
+        <input onChange="checking(e)" style={{border: '0', padding: '10px', borderRadius: '5px', borderLeft: '3px solid green', backgroundColor: 'black', color: 'white'}} id="github-id" placeholder="UserName" /><br /><br />
         <button onClick={check}>Check!</button><br /><br />
         <div id="profile" style={{display: 'none'}}><h4>Profile</h4>
         Name: <span id="name"></span><br />
