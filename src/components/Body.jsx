b6439f8851a939ad753ee6c70748db49cc62e8b4
Mushahid Ali <30910296+MushahidAli@users.noreply.github.com>
2023-04-25 10:04:22 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const Body = () => {
-    function checking(e) { 
-        alert('first'+e.target.key) 
-        if (e.target.key === 'Enter') { 
-            alert('second') 
-            check();
-        }
-    }
+    var input = document.getElementById("github-id");
+input.addEventListener("keypress", function(event) {
+  alert("First "+event.key);
+  if (event.key === "Enter") {
+    alert("Second "+event.key);
+  }
+});
     var users = [];
     var repolist = [];
     async function check() {
@@ -62,7 +62,7 @@ const Body = () => {
 
     return(<><div style={{color: 'black', border: '1px solid grey', borderRadius: '5px', backgroundColor: '#D3D3D3'}}>
         <br />GitHub UserName:<br /><br />
-        <input onChange={checking} style={{border: '0', padding: '10px', borderRadius: '5px', borderLeft: '3px solid green', backgroundColor: 'black', color: 'white'}} id="github-id" placeholder="UserName" /><br /><br />
+        <input style={{border: '0', padding: '10px', borderRadius: '5px', borderLeft: '3px solid green', backgroundColor: 'black', color: 'white'}} id="github-id" placeholder="UserName" /><br /><br />
         <button onClick={check}>Check!</button><br /><br />
         <div id="profile" style={{display: 'none'}}><h4>Profile</h4>
         Name: <span id="name"></span><br />
